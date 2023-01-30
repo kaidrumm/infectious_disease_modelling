e6436c1ac011a62b2253b1da1efc38812fbca487
kaidrumm <kaidrummz@gmail.com>
2023-01-30 11:04:40 -0700

    Initial template

---
+++ homework1/SIRmodel_forwardEuler.py
@@ -0,0 +1,8 @@
+import matplotlib
+
+def forward_euler(N, I, S, beta, gamma):
+    return
+
+forward_euler(1000, 1, 999, 1, 0.5)
+forward_euler(1000, 1, 999, 1.5, 0.5)
+forward_euler(1000, 1, 999, 2, 0.5)
